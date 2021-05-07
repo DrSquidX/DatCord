7e0b0505fbbe473871e74d6dc5edbcabb62ff9ac
DrSquidX <68380622+DrSquidX@users.noreply.github.com>
2021-05-07 21:07:40 +0200

    Can now save user info for later use

--- DatCordClient.py
+++ DatCordClient.py
@@ -1,4 +1,4 @@
-import socket, threading, sqlite3
+import socket, threading, sqlite3, os, sys
 class Client:
     """Configures the database files if they don't exist, as well as
     also configuring the database tables if they also don't exist.
@@ -19,23 +19,33 @@ class Client:
             cursor.execute("select * from servers")
         except:
             cursor.execute("create table servers(server)")
+        try:
+            cursor.execute("select * from userinfo")
+        except:
+            cursor.execute("create table userinfo(username, password)")
+        try:
+            cursor.execute("select * from userinfo")
+            self.userinfo = cursor.fetchall()[0]
+        except:
+            self.userinfo = None
+        self.userinfo1 = self.userinfo
         db.commit()
         cursor.close()
         db.close()
         self.join_serv()
     def join_serv(self):
-        """This is the function that is used to connect to the server. If first prompts the user 
-        if they want to connect to an existing server in their directory, or if they want to 
-        connect to a new server that they haven't connected to one yet. If the user says 'yes', 
-        the database file with all of the servers and IP's will be displayed. If there isn't 
+        """This is the function that is used to connect to the server. If first prompts the user
+        if they want to connect to an existing server in their directory, or if they want to
+        connect to a new server that they haven't connected to one yet. If the user says 'yes',
+        the database file with all of the servers and IP's will be displayed. If there isn't
         anything in the database, it will tell the user that there isn't any server to connect
-        to. If there is stuff in the database file, then the user can select which server that 
+        to. If there is stuff in the database file, then the user can select which server that
         they want to connect to. They can then be able to connect to that server. If the user wants
-        to join a new server, they will be prompted to enter the IP and Port of the new server. 
+        to join a new server, they will be prompted to enter the IP and Port of the new server.
         If the user can connect to that IP and Port, it will be added to the server database
         files, and you can connect to it with ease when you want to again."""
         self.inserver = False
-        print("\n[+] You are required to go connect to a DatCord Server, before you are able to communicate with others.")
+        print("\n[+] Connect To A Server\n[+] You are required to go connect to a DatCord Server, before you are able to communicate with others.")
         while True:
             connect = input("\n[+] Would you like to connect to an existing server or create a new profile for one?(yes/no): ")
             if connect.lower().strip() == "yes":
@@ -126,43 +136,75 @@ Client Script For DatCord by DrSquid"""
         while True:
             try:
                 if not self.logged_in:
-                    username = input("\n[+] Enter your username: ")
-                    password = input("[+] Enter your password: ")
-                    self.client.send(f"!login {username} {password}".encode())
-                    msg = self.client.recv(10240).decode()
-                    if "Successfully logged in!" in msg or "Commands For This Server" in msg:
-                        print("\n[+] Successfully logged into your account.")
-                        print("[+] You are able to communicate with users on Datcord now.")
-                        self.logged_in = True
-                    elif "Authentication Failed." in msg:
-                        print("[+] Your password is invalid. Please check for spelling errors.")
-                    elif "Your account is not registered in the database. Please register your account." in msg:
-                        print("[+] Your account is not recognized by the database.")
-                        register = input("[+] Would you like to register an account with the details provided?: ")
-                        if register.lower() == "yes":
-                            self.client.send(f"!register {username} {password}".encode())
-                            self.logged_in = True
+                    if self.userinfo is not None:
+                        useinfo = input(f"\n[+] Would you like to use the same userinfo from last time you logged in?: ")
+                        if useinfo.lower().strip() == "yes":
+                            self.client.send(f"!login {self.userinfo[0]} {self.userinfo[1]}".encode())
+                            msg = self.client.recv(10240).decode()
+                            if "Successfully logged in!" in msg or "Commands For This Server" in msg:
+                                print("\n[+] Successfully logged into your account.")
+                                print("[+] You are able to communicate with users on Datcord now.")
+                                self.reciever = threading.Thread(target=self.recv)
+                                self.reciever.start()
+                                self.logged_in = True
+                            else:
+                                print("[+] There was an error with logging you in. Switching to normal logging in.")
+                                self.userinfo = None
                         else:
-                            print("[+] Please re-enter your crentials.")
-                    elif "Your account has been banned from the server." in msg:
-                        print("[+] Your account has been banned from the server! Try to create a new account or wait until you are unbanned.")
+                            print("[+] Choosing not to use the same info.")
+                            self.userinfo = None
                     else:
-                        print("[+] Unable to login.")
-                        print(msg)
+                        username = input("\n[+] Enter your username: ")
+                        password = input("[+] Enter your password: ")
+                        self.client.send(f"!login {username} {password}".encode())
+                        msg = self.client.recv(10240).decode()
+                        if "Successfully logged in!" in msg or "Commands For This Server" in msg:
+                            print("\n[+] Successfully logged into your account.")
+                            print("[+] You are able to communicate with users on Datcord now.")
+                            self.logged_in = True
+                        elif "Authentication Failed." in msg:
+                            print("[+] Your password is invalid. Please check for spelling errors.")
+                        elif "Your account is not registered in the database. Please register your account." in msg:
+                            print("[+] Your account is not recognized by the database.")
+                            register = input("[+] Would you like to register an account with the details provided?: ")
+                            if register.lower() == "yes":
+                                self.client.send(f"!register {username} {password}".encode())
+                                self.logged_in = True
+                            else:
+                                print("[+] Please re-enter your crentials.")
+                        elif "Your account has been banned from the server." in msg:
+                            print("[+] Your account has been banned from the server! Try to create a new account or wait until you are unbanned.")
+                        else:
+                            print("[+] Unable to login.")
+                            print(msg)
+                        if self.logged_in:
+                            db = sqlite3.connect(self.dbfile)
+                            cursor = db.cursor()
+                            if self.userinfo1 is None:
+                                cursor.execute(f"insert into userinfo values('{username}','{password}')")
+                            else:
+                                cursor.execute(f"delete from userinfo where username = '{self.userinfo1[0]}'")
+                                cursor.execute(f"insert into userinfo values('{username}','{password}')")
+                            self.userinfo = (username, password)
+                            self.userinfo1 = self.userinfo
+                            db.commit()
+                            cursor.close()
+                            db.close()
+                            self.reciever = threading.Thread(target=self.recv)
+                            self.reciever.start()
+                        print("")
                     if self.logged_in:
-                        self.reciever = threading.Thread(target=self.recv)
-                        self.reciever.start()
-                    print("")
+                        print(f"\n[+] Logged in as: {self.userinfo1[0]}")
                 else:
                     msg = input("[+] Enter your msg: ")
                     self.client.send(msg.encode())
-            except:
+            except Exception as e:
                 print("[+] There appears to be some issues with your connection with the server.")
                 servjoiner = threading.Thread(target=self.join_serv).start()
                 break
     def recv(self):
         """This function is what the client uses to recieve messages from the server."""
-        print("[+] You are free to send commands to the server.\n[+] You can run the commands that are used by the server(if you know them) or use the commands on this script to communicate with it.")
+        print("\n[+] You are free to send commands to the server.\n[+] You can run the commands that are used by the server(if you know them) or use the commands on this script to communicate with it.")
         while True:
             try:
                 msg = self.client.recv(10240).decode()
@@ -177,7 +219,15 @@ Client Script For DatCord by DrSquid"""
                         result = result + i + " "
                     print(f"[+] From {username.strip()} {result}")
                 else:
-                    print(msg)
+                    if msg.strip() == "":
+                        pass
+                    else:
+                        print(msg)
             except:
                 break
-client = Client()
+if __name__ == '__main__':
+    if sys.platform == "win32":
+        os.system("cls")
+    else:
+        os.system("clear")
+    client = Client()
